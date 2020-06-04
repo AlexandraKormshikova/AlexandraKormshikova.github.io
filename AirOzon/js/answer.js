2f85293be45ee433cabb5dcd3a152774d7f46739
AlexandraKormshikova <alexweasley06@gmail.com>
2020-06-05 01:50:58 +0300

    made all site without popup

---
+++ AirOzon/js/answer.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.answer_arrow1').click(function(event){
+        $('.answer_text_grey1').toggleClass('none');
+    });
+
+});
+$(document).ready(function(){
+    $('.answer_arrow2').click(function(event){
+        $('.answer_text_grey2').toggleClass('none');
+    });
+
+});

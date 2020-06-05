fb64d80fd216b191c83050d29ca10665c56bfc9f
AlexandraKormshikova <alexweasley06@gmail.com>
2020-06-05 12:04:58 +0300

    footer

--- AirOzon/js/answer.js
+++ AirOzon/js/answer.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $('.answer_arrow1').click(function(event){
-        $('.answer_text_grey1').toggleClass('none');
+        $('.answer_text_grey1, .answer_arrow1').toggleClass('none');
     });
 
 });
 $(document).ready(function(){
     $('.answer_arrow2').click(function(event){
-        $('.answer_text_grey2').toggleClass('none');
+        $('.answer_text_grey2, .answer_arrow2').toggleClass('none');
     });
 
 });

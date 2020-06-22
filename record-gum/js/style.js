27992cee23dbba5981650f530504d529a049e96e
AlexandraKormshikova <alexweasley06@gmail.com>
2020-06-22 19:59:05 +0300

    vh

---
+++ record-gum/js/style.js
@@ -0,0 +1,7 @@
+document.getElementById('mob_menu').onclick = function() {
+  document.getElementById('mob_menu_body').classList.remove('none');
+}
+
+document.getElementById('mob_menu_close').onclick = function() {
+  document.getElementById('mob_menu_body').classList.add('none');
+}

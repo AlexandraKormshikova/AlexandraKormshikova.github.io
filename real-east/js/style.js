6870d7b2129be8969aa36dcbd7ea8afb688e29cb
AlexandraKormshikova <alexweasley06@gmail.com>
2020-06-28 20:04:33 +0300

    bdbs

--- real-east/js/style.js
+++ real-east/js/style.js
@@ -1,3 +1,13 @@
 document.getElementById('mob_menu').onclick = function() {
   document.getElementById('header_mob').classList.toggle('none');
 }
+
+document.getElementById('type_list_cols').onclick = function() {
+  document.getElementById('search').classList.add('search2');
+  document.getElementById('search').classList.remove('search1');
+}
+
+document.getElementById('type_list_rows').onclick = function() {
+  document.getElementById('search').classList.add('search1');
+  document.getElementById('search').classList.remove('search2');
+}

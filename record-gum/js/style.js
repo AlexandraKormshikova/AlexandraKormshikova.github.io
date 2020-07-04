ab26281b6b639171f1d85cdc6e21f7c2b093aea9
AlexandraKormshikova <alexweasley06@gmail.com>
2020-07-04 19:31:28 +0300

    yt

--- record-gum/js/style.js
+++ record-gum/js/style.js
@@ -5,3 +5,16 @@ document.getElementById('mob_menu').onclick = function() {
 document.getElementById('mob_menu_close').onclick = function() {
   document.getElementById('mob_menu_body').classList.add('none');
 }
+
+window.addEventListener('scroll', function() {
+  pageYOffset >= 80 ?
+    document.getElementById('header').classList.add('header_black') :
+    document.getElementById('header').classList.remove('header_black');
+});
+
+window.addEventListener('scroll', function() {
+  let width = document.body.clientWidth;
+  pageYOffset >= 400 && (width > 820) ?
+    document.getElementById('over_header').classList.add('over_header_hidden') :
+    document.getElementById('over_header').classList.remove('over_header_hidden');
+});
